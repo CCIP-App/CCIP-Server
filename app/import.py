e75949c254146731114e800fff559d9a64fe208c
Denny Huang <denny0223@gmail.com>
2017-02-26 07:30:01 +0800

    Staff import support use scenario json file

--- app/import.py
+++ app/import.py
@@ -14,10 +14,7 @@ def str2timestamp(str):
     return datetime.strptime(str, "%Y/%m/%d %H:%M %z").timestamp()
 
 
-def bind_scenario(row, attendee, scenario_filename):
-    with open(scenario_filename) as scenario_file:
-        scenarios = json.load(scenario_file)
-
+def bind_scenario(row, attendee, scenarios):
     for scenario_id, scenario in scenarios.items():
         sce = Scenario()
         sce.order = scenario['order']
@@ -47,7 +44,7 @@ def bind_scenario(row, attendee, scenario_filename):
     attendee.save()
 
 
-def list_import(attendee_list, scenario_filename):
+def list_import(attendee_list, scenarios):
     for row in attendee_list:
         attendee = Attendee()
         attendee.token = row['token']
@@ -56,10 +53,10 @@ def list_import(attendee_list, scenario_filename):
         else:
             attendee.user_id = row['id']
 
-        bind_scenario(row, attendee, scenario_filename)
+        bind_scenario(row, attendee, scenarios)
 
 
-def staff_import(attendee_list):
+def staff_import(attendee_list, scenarios):
     for row in attendee_list:
         attendee = Attendee()
         attendee.token = row['username']
@@ -76,66 +73,11 @@ def staff_import(attendee_list):
         attendee.attr['teams'] = teams
         attendee.attr['title'] = row['title']
 
-        sce1 = Scenario()
-        sce1.order = 1
-        sce1.available_time = str2timestamp("2016/08/20 8:30")
-        sce1.expire_time = str2timestamp("2016/08/20 15:00")
-        sce1.countdown = 0
-        attendee.scenario['day1checkin'] = sce1
-
-        sce2 = Scenario()
-        sce2.order = 2
-        sce2.available_time = str2timestamp("2016/08/20 8:30")
-        sce2.expire_time = str2timestamp("2016/08/20 15:00")
-        sce2.disabled = "Haven't Check-in"
-        sce2.countdown = 60
-        sce2.attr['shirt_size'] = row['shirt_size']
-        attendee.scenario['kit'] = sce2
-
-        sce3 = Scenario()
-        sce3.order = 3
-        sce3.available_time = str2timestamp("2016/08/20 11:30")
-        sce3.expire_time = str2timestamp("2016/08/20 14:00")
-        sce3.disabled = "Haven't Check-in"
-        sce3.countdown = 60
-        if row['diet'] == '葷':
-            sce3.attr = {"diet": "meat"}
-        else:
-            sce3.attr = {"diet": "vegetarian"}
-        attendee.scenario['day1lunch'] = sce3
-
-        sce4 = Scenario()
-        sce4.order = 4
-        sce4.available_time = str2timestamp("2016/08/21 8:30")
-        sce4.expire_time = str2timestamp("2016/08/21 15:00")
-        sce4.countdown = 0
-        attendee.scenario['day2checkin'] = sce4
-
-        sce5 = Scenario()
-        sce5.order = 5
-        sce5.available_time = str2timestamp("2016/08/21 11:30")
-        sce5.expire_time = str2timestamp("2016/08/21 14:00")
-        sce5.disabled = "Haven't Check-in"
-        sce5.countdown = 60
-        if row['diet'] == '葷':
-            sce5.attr = {"diet": "meat"}
-        else:
-            sce5.attr = {"diet": "vegetarian"}
-        attendee.scenario['day2lunch'] = sce5
-
-        sce6 = Scenario()
-        sce6.order = 6
-        sce6.available_time = str2timestamp("2016/08/20 8:30")
-        sce6.expire_time = str2timestamp("2016/08/21 15:00")
-        sce6.disabled = "For Supporters Only"
-        sce6.countdown = 60
-        attendee.scenario['vipkit'] = sce6
-
-        attendee.save()
+        bind_scenario(row, attendee, scenarios)
 
 
-def from_csv(csv_file, staff=False):
-    list_import(csv.DictReader(csv_file), scenario_filename) if not staff else staff_import(csv.DictReader(csv_file))
+def from_csv(csv_file, scenarios, staff=False):
+    list_import(csv.DictReader(csv_file), scenarios) if not staff else staff_import(csv.DictReader(csv_file), scenarios)
 
 
 if __name__ == '__main__':
@@ -148,5 +90,6 @@ if __name__ == '__main__':
     except IndexError:
         staff = False
 
-    with open(filename, 'r') as csv_file:
-        from_csv(csv_file, staff)
+    with open(filename, 'r') as csv_file, open(scenario_filename, 'r') as scenario_file:
+        scenarios = json.load(scenario_file)
+        from_csv(csv_file, scenarios, staff)
