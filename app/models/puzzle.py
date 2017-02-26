2c0b3da3c16445ade712d80723324b60acae7d62
Denny Huang <denny0223@gmail.com>
2017-02-27 05:29:32 +0800

    Add new model PuzzleBucket

--- app/models/puzzle.py
+++ app/models/puzzle.py
@@ -1,5 +1,18 @@
 from models import db
+from models import Attendee
 
 
 class PuzzleStatus(db.Document):
     puzzle = db.DictField()
+
+
+class PuzzleBucket(db.Document):
+    attendee = db.ReferenceField(Attendee)
+    public_token = db.StringField()
+    puzzle = db.ListField()
+
+    meta = {
+        'indexes': [
+            'public_token'
+        ]
+    }
